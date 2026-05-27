da048b430c1f1963eca675d1154089a7a618d0c8
Jhosue Rui <ingrui@gmail.com>
2026-05-27 15:36:45 -0400

    ejemplo de modificacion para caso open close

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,11 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 export class Httpclient{
 
+    // async get(url:string){
+    //     const {data, status} = await axios.get(url);
+    //     //console.log({status});
+    //     return {data,status};
+    // }
+
     async get(url:string){
-        const {data, status} = await axios.get(url);
-        //console.log({status});
-        return {data,status};
+        const resp = await fetch(url);
+        const data = await resp.json();
+        return {data, status: resp.status};
     }
 
 }
